d56b8244b908c343dda5c5b8d1ce133ec0fefb1f
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-08 14:52:12 +0300

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textArea: document.querySelector(".feedback-form textarea"),
+    inputEl: document.querySelector(".feedback-form input")
+}
+const KEYMSG = "message"
+const KEYEMAIL = "email"
+const formData =[]
+refs.form.addEventListener("submit", onFormSubmit);
+refs.textArea.addEventListener("input", throttle(onClickInput, 500))
+// refs.textArea.addEventListener("input", onClickInput)
+
+refs.form.addEventListener("input", e => {
+   
+    formData[e.target.name] = e.target.value
+    localStorage.setItem([e.target.name], e.target.value)
+})
+
+
+
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    e.currentTarget.reset()
+    localStorage.removeItem(KEYMSG)
+    localStorage.removeItem(KEYEMAIL)
+}    
+function onClickInput(e) {
+
+    localStorage.setItem(KEYMSG, value)
+
+}
+
+
+textAreaSave()
+function textAreaSave() {
+    console.log(1)
+    const saveMasege = localStorage.getItem(KEYMSG)
+    const saveEmail = localStorage.getItem(KEYEMAIL)
+    if (saveMasege || saveEmail) {
+        refs.textArea.value = saveMasege
+        refs.inputEl.value = saveEmail
+    }
+}
