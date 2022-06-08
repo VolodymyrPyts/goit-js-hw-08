65e8ac6a8f85cd4944462598544423da0eec965a
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-08 10:21:45 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+document.querySelector(".gallery").insertAdjacentHTML(
+    'beforeend',
+    galleryItems.map(({ preview, original, description }) => {
+        
+        return `<a class="gallery__item" href = "${original}" >
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`}).join(""));
+
+
+// new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionPosition: 'bottom',
+//     captionDelay: 250,
+// });
