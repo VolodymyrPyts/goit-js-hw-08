0ff8654d20ea5f8191fbafff05e4e3189fb6ffe6
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-08 22:22:56 +0300

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,7 +7,7 @@ document.querySelector(".gallery").insertAdjacentHTML(
     'beforeend',
     galleryItems.map(({ preview, original, description }) => {
         
-        return `<a class="gallery__item" href = "${original}" >
+        return `<a class="gallery__item" href="${original}" >
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`}).join(""));
 
