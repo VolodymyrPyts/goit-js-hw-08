c557bce9c11cf7573438129d1b8bd5d819604ea6
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-10 09:15:19 +0300

    Task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
 
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Vimeo(document.querySelector('#vimeo-player'));
+const VIDEO_CURR_TIME = 'videoplayer-current-time';
+
+if (localStorage.getItem(VIDEO_CURR_TIME)) {
+  player
+    .setCurrentTime(JSON.parse(localStorage.getItem(VIDEO_CURR_TIME)))
+    .catch(err => {
+      switch (err.name) {
+        case 'RangeError':
+          console.error(
+            'The time was less than 0 or greater than the video’s duration.'
+          );
+          player.setCurrentTime(0);
+          break;
+
+        default:
+          console.error(err.name);
+          break;
+      }
+    });
+}
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(VIDEO_CURR_TIME, e.seconds);
+  }, 1000)
+);
